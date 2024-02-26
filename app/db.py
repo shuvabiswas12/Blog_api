ff163dd370a4b6839d5166b1d5ddb48a1bf62321
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-02-26 11:48:35 +0600

    db configuration added.

---
+++ app/db.py
@@ -0,0 +1,20 @@
+import os
+from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
+
+mongodb_url = os.getenv('DB_URL')
+
+# create a new client and connect to the server
+CLIENT = MongoClient(mongodb_url, server_api=ServerApi('1'))
+
+DB = CLIENT.get_database("fastapi__blog_api")
+
+# collections
+blogs_collection = DB.get_collection("blogs")
+
+# send a ping to confirm a successful connection
+try:
+    CLIENT.admin.command('ping')
+    print("App is successfully connected to the database mongoDB!")
+except Exception as e:
+    print(e)
