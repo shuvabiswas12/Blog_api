afad75e217e818a2815b334a0ef3af0035dcf05a
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-03-02 01:10:24 +0600

    modified datetime field is optional.

--- app/schemas/like_schema.py
+++ app/schemas/like_schema.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, Field
 
 
 class Like(BaseModel):
     blog_id: str
     user_id: str
-    created_at: datetime
+    created_at: Optional[datetime] = Field(default=datetime.now())
